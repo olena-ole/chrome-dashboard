b45c986f778e19f50b7eeeda8970dc8dc532887b
Lenna <pampam32121@gmail.com>
2022-07-01 02:26:05 +0300

    set default bg image in case of promise rejection

--- index.js
+++ index.js
@@ -6,8 +6,14 @@ function setBackgroundImage() {
     fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature')
         .then(res => res.json())
         .then(data => {
+            console.log(data.urls.full)
+            console.log(data.user.name)
             document.body.style.backgroundImage = `url(${data.urls.full})`; 
             authorEl.textContent = `By: ${data.user.name}`;
+        })
+        .catch(err => {
+            document.body.style.backgroundImage = 'url(https://images.unsplash.com/photo-1473448912268-2022ce9509d8?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTY2MzExODM&ixlib=rb-1.2.1&q=80)'; 
+            authorEl.textContent = 'By: Luca Bravo';
         });
 }
 
