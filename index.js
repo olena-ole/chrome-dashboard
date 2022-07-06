961d528059929de89fd7b4a20fbf35e004c53bba
Lenna <pampam32121@gmail.com>
2022-07-06 16:25:56 +0300

    added current time display

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const authorEl = document.querySelector('#author');
 const cryptoTitleEl = document.querySelector('#crypto-title');
 const cryptoPricesEl = document.querySelector('#crypto-prices');
+const timeEl = document.querySelector('#time');
+
 
 function setBackgroundImage() {
     fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature')
@@ -15,26 +17,35 @@ function setBackgroundImage() {
             document.body.style.backgroundImage = 'url(https://images.unsplash.com/photo-1473448912268-2022ce9509d8?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTY2MzExODM&ixlib=rb-1.2.1&q=80)'; 
             authorEl.textContent = 'By: Luca Bravo';
         });
-}
+};
+
+function displayDogecoinRate() {
+    fetch('https://api.coingecko.com/api/v3/coins/dogecoin')
+        .then(res => {
+            if (!res.ok) {
+                throw Error('something went wrong');
+            }
+            return res.json();
+        })
+        .then(data => {
+            cryptoTitleEl.innerHTML = `
+                <img src="${data.image.small}" alt="${data.name} icon">
+                <span>${data.name}</span>
+            `;
+            cryptoPricesEl.innerHTML = `
+                <p>🎯: $${data.market_data.current_price.usd}</p>
+                <p>👆: $${data.market_data.high_24h.usd}</p>
+                <p>👇: $${data.market_data.low_24h.usd}</p>
+            `;
+        })
+        .catch(err => console.error(err));
+};
+
+function displayCurrentTime() {
+    const time = new Date();
+    timeEl.textContent = time.toLocaleString('en-US', {timeStyle: "short"});
+};
 
 setBackgroundImage();
-
-fetch('https://api.coingecko.com/api/v3/coins/dogecoin')
-    .then(res => {
-        if (!res.ok) {
-            throw Error('something went wrong');
-        }
-        return res.json();
-    })
-    .then(data => {
-        cryptoTitleEl.innerHTML = `
-            <img src="${data.image.small}" alt="${data.name} icon">
-            <span>${data.name}</span>
-        `;
-        cryptoPricesEl.innerHTML = `
-            <p>🎯: $${data.market_data.current_price.usd}</p>
-            <p>👆: $${data.market_data.high_24h.usd}</p>
-            <p>👇: $${data.market_data.low_24h.usd}</p>
-        `;
-    })
-    .catch(err => console.error(err));
+displayDogecoinRate();
+setInterval(displayCurrentTime, 1000);
